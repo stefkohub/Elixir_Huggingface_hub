a79ea2f9560733168fce71ebcecf75e174acc2b4
stefkohub <ste.falsetto@gmail.com>
2022-02-09 15:51:08 +0100

    Removed not useful files

--- lib/huggingface_hub/modeltags.ex
+++
@@ -1,9 +0,0 @@
-defmodule Huggingface_hub.ModelTags do
-  use Huggingface_hub.GeneralTags
-
-  def get_tags(tag_dictionary) do
-    {pid, state} = start_link(tag_dictionary)
-    IO.puts("Adesso state=#{inspect(state)}")
-    state
-  end
-end
